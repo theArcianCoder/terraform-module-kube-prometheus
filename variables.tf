67428f263baed8ead25a39f28de300ac3eb316c0
theArcianCoder <65061393+theArcianCoder@users.noreply.github.com>
2023-07-14 15:02:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "namespace" {
   description = "The name of the Kubernetes namespace."
   type        = string
 }
-variable "version" {
-  description = "The version of the Helm chart release."
-  type        = string
-}
 variable "stack_name" {
   description = "The name of the Helm chart release."
   type        = string
