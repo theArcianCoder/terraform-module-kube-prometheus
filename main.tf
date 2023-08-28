f71a255ecb9dfb4445e670519564bc17dcad8aa5
theArcianCoder <countingcr223@gmail.com>
2023-08-28 12:44:38 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "helm_release" "kube-prometheus" {
 
   name       = var.stack_name
   namespace  = var.namespace    
-  repository = "https://raw.githubusercontent.com/theArcianCoder/helm-chart/main"
+  repository = "https://raw.githubusercontent.com/theArcianCoder/helm-chart-ttn/main"
   chart      = "kube-prometheus-stack"
 
   set {
