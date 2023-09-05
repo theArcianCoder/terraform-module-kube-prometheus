01263072c83dd907f083c891842abfffd1212b72
theArcianCoder <countingcr223@gmail.com>
2023-09-05 17:44:39 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -15,6 +15,13 @@ provider "kubectl" {
       host                   = data.aws_eks_cluster.cluster.endpoint
       cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
 resource "kubernetes_namespace" "monitoring" {
   depends_on = [
     var.eks_cluster_id
