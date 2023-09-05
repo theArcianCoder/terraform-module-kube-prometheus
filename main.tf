a8e3bb60578053506f3615c3d54ca40d77cde48a
theArcianCoder <countingcr223@gmail.com>
2023-09-05 17:51:40 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -15,11 +15,8 @@ provider "kubectl" {
       host                   = data.aws_eks_cluster.cluster.endpoint
       cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
 
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
