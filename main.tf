ac81b028f74dad8de4d8abb59f3ec8bfd5732761
theArcianCoder <countingcr223@gmail.com>
2023-09-06 10:06:58 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -8,15 +8,21 @@ terraform {
 }
 
 provider "kubectl" {  
-  host = module.eks_cluster.cluster_endpoint  
-  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)  
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data) 
   exec {    
     api_version = "client.authentication.k8s.io/v1beta1"    
     command     = "aws"    
     args = ["eks", "get-token", "--cluster-name", local.workspace.eks_cluster.name,"--role-arn" ,"arn:aws:iam::${local.workspace.aws.account_id}:role/${local.workspace.aws.role}" ]  
     } 
 }
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
 resource "kubernetes_namespace" "monitoring" {
   depends_on = [
     var.eks_cluster_id
