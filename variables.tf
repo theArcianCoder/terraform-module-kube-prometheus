ecdc693decc8215cc0794212f66d0a4fa491030a
theArcianCoder <countingcr223@gmail.com>
2023-09-08 15:21:21 +0530

    Test Changes

--- variables.tf
+++ variables.tf
@@ -16,11 +16,3 @@ variable "ebs_volume_id" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
 }
-variable "target1" {
-  description = "The first target IP and port"
-  default     = "18.236.158.154:9114"
-}
-variable "target2" {
-  description = "The second target IP and port"
-  default     = "18.236.158.154:9216"
-}
