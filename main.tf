14695c90a925ceb1670966ff672c116fd77bda37
theArcianCoder <countingcr223@gmail.com>
2023-09-18 15:41:36 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -150,9 +150,9 @@ resource "helm_release" "kube-prometheus" {
     value = "${var.target2}"
   }
 }
+
 resource "kubectl_manifest" "grafana-config" {
   depends_on = [helm_release.kube-prometheus]
-
   yaml_body = <<YAML
 apiVersion: v1
 kind: ConfigMap
@@ -187,7 +187,6 @@ YAML
 
 resource "kubectl_manifest" "grafana-config-binding" {
   depends_on = [kubectl_manifest.grafana-config]
-
   yaml_body = <<YAML
 apiVersion: apps/v1
 kind: Deployment
